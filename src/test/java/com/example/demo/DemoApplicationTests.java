0e43864c3282d70d609a33a2baf72068769446a0
willis <willis@getinfo.net.br>
2021-02-13 10:51:40 -0300

    Adicionando testes e um jenkinsfile

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,46 @@
 package com.example.demo;
 
+import com.example.demo.controller.HomeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private HomeController controller;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc
+                .perform(get("/")).andDo(print())
+                .andExpect(status().isOk()).andExpect(content().string(equalTo("Hello World!")));
+    }
+
+    @Test
+    public void shouldReturnWillisMessage() throws Exception {
+        this.mockMvc
+                .perform(get("/?name=willis")).andDo(print())
+                .andExpect(status().isOk()).andExpect(content().string(equalTo("Hello willis!")));
+    }
 
 }
